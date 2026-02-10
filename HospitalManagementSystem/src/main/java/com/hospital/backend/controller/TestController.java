29e92b2f7d9608fa1cbdd0f3731a98e95af669aa
SUDHIRsaa <sudmaurya2611@gmail.com>
2026-02-10 23:34:56 +0530

    ok

---
+++ HospitalManagementSystem/src/main/java/com/hospital/backend/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.hospital.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/db-test")
+    public String test() {
+        jdbcTemplate.execute("SELECT 1");
+        return "DB Connected!";
+    }
+}
